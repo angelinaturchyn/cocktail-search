dcd1fe3a78c37ec19cbc570a4cb73e76a918bdad
angelinaturchyn <angelinaturchyn@gmail.com>
2023-03-11 00:56:38 -0600

    added error statement

--- src/components/cocktailSearch.js
+++ src/components/cocktailSearch.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 export const CocktailSearch = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [cocktails, setCocktails] = useState([]);
+    const [error, setError] = useState("");
 
     const handleInputChange = (event) => {
         setSearchTerm(event.target.value);
@@ -11,28 +12,55 @@ export const CocktailSearch = () => {
     const searchCocktailByName = () => {
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
             .then((response) => response.json())
-            .then((data) => setCocktails(data.drinks))
+            .then((data) => {
+                if (data.drinks) {
+                    setCocktails(data.drinks);
+                    setError("");
+                } else {
+                    setError("Whoops, try a different name");
+                }
+            })
             .catch((error) => console.log(error));
     };
 
     const searchCocktailByIngredient = (ingredient) => {
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
             .then((response) => response.json())
-            .then((data) => setCocktails(data.drinks))
+            .then((data) => {
+                if (data.drinks) {
+                    setCocktails(data.drinks);
+                    setError("");
+                } else {
+                    setError("Whoops, try a different name");
+                }
+            })
             .catch((error) => console.log(error));
     };
 
     const searchCocktailByCategory = (category) => {
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${category}`)
             .then((response) => response.json())
-            .then((data) => setCocktails(data.drinks))
+            .then((data) => {
+                if (data.drinks) {
+                    setCocktails(data.drinks);
+                    setError("");
+                } else {
+                    setError("Whoops, try a different name");
+                }
+            })
             .catch((error) => console.log(error));
     };
-
     const searchCocktailByFirstLetter = (letter) => {
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
             .then((response) => response.json())
-            .then((data) => setCocktails(data.drinks))
+            .then((data) => {
+                if (data.drinks) {
+                    setCocktails(data.drinks);
+                    setError("");
+                } else {
+                    setError("Whoops, try a different name");
+                }
+            })
             .catch((error) => console.log(error));
     };
 
@@ -59,7 +87,7 @@ export const CocktailSearch = () => {
     return (
         <div>
             <div>
-                <input type="text" value={searchTerm} onChange={handleInputChange} />
+                <input type="text" value={searchTerm} onChange={handleInputChange}/>
                 <button onClick={handleSearchByName}>Search by Name</button>
                 <button onClick={handleSearchByIngredient}>Search by Ingredient</button>
                 <button onClick={handleSearchByAlcoholic}>Alcoholic</button>
@@ -70,15 +98,19 @@ export const CocktailSearch = () => {
                     </button>
                 ))}
             </div>
-            <div>
-                {cocktails.map((cocktail) => (
-                    <div key={cocktail.idDrink}>
-                        <h3>{cocktail.strDrink}</h3>
-                        <img src={cocktail.strDrinkThumb} alt="cocktail" />
-                        <p>Instructions: {cocktail.strInstructions}</p>
-                    </div>
-                ))}
-            </div>
+            {error ? (
+                <div>Whoops, try a different name</div>
+            ) : (
+                <div>
+                    {cocktails.map((cocktail) => (
+                        <div key={cocktail.idDrink}>
+                            <h3>{cocktail.strDrink}</h3>
+                            <img src={cocktail.strDrinkThumb} alt="cocktail"/>
+                            <p>Instructions: {cocktail.strInstructions}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
-};
+}
